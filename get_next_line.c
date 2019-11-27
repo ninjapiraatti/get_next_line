c0baf92b412e62b969f558cbbf721d242a86a2c6
Tuomas Louekari <tlouekar@c2r4p8.hive.fi>
2019-11-27 21:05:34 +0200

    Still no go

--- get_next_line.c
+++ get_next_line.c
@@ -6,94 +6,108 @@
 /*   By: tlouekar <tlouekar@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 11:09:08 by tlouekar          #+#    #+#             */
-/*   Updated: 2019/11/19 12:52:46 by tlouekar         ###   ########.fr       */
+/*   Updated: 2019/11/27 21:04:06 by tlouekar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "get_next_line.h"
 /*
-char	*youhadonejob(const char *s)
+int		returnhandler(int fd, char **strs_handler, char **line)
 {
-	char	*temp;
+	// Returnhandler is supposed to write the stuff before \n to *line, then write the rest if needed.
+	// Return 1, 0, or -1 (when -1 though?)
+	// After you find \n, you also have to check if the rest of the buffer has \0 before end?
 	int		i;
-	int		len;
-	int		nlfound;
 
 	i = 0;
-	nlfound = 0;
-	len = 0;
-	strs[fd] = ft_strjoin(strs[fd], temp);
-	while (s[len] != '\0')
-	{
-		if (s[len] == '\n')
-		{
-			nlfound = 1;
-			break ;
-		}
-		len++;
-	}
-	if (nlfound == 1)
-	{
-		if (!(temp = (char *)malloc(len * sizeof(char) + 1)))
-			return (0);
-		while (i < len)
-		{
-			temp[i] = s[i];
-			i++;
-		}
-		return (temp);
-	}
-	return ((char *)s);
+	while (strs_handler[fd][i] != '\n')
+		i++;
+	if (!(*line = ft_strsub(strs_handler[fd], 0, i)))
+		return (-1);
+	if (!(strs_handler[fd] = ft_strdup(strs_handler[fd] + i + 1)))
+		return (-1);
+	return (1);
 }
 */
 
-int		youhadonejob(int fd, char *trimstr, char **strs_onejob, char **line)
+int		linejoiner(int fd, char *buffstr, char **strs_joiner, char **line)
 {
 	int		i;
 	char	*temp;
-	int		len;
 
 	i = 0;
-	len = 0;
-	temp = ft_strnew(BUFF_SIZE);
-	while(trimstr[i] != '\n' && trimstr[i] != '\0')
-		len++;
-	*line = ft_strsub(trimstr, 0, len);
-	strs_onejob[fd] = ft_strjoin(strs_onejob[fd], temp);
+	if ((buffstr[0] == '\0') && (!strs_joiner[fd]))
+		return (0);
+	//*line = ft_strjoin(*line, strs_joiner[fd]);
+	strs_joiner[fd] = ft_strnew(BUFF_SIZE);
+	while(buffstr[i] != '\0')
+	{
+		if (buffstr[i] == '\n')
+		{
+			*line = ft_strjoin(*line, ft_strsub(buffstr, 0, i)); // Joining not necessary, line empty at this point?
+			temp = ft_strdup(buffstr + i + 1);
+			strs_joiner[fd] = ft_strjoin(strs_joiner[fd], temp);
+			ft_strdel(&temp);
+			return (0);
+		}
+		i++;
+	}
+	if (i == BUFF_SIZE)
+	{
+		*line = ft_strjoin(*line, ft_strsub(buffstr, 0, i));
+		return (1);
+	}
+	*line = ft_strjoin(*line, ft_strsub(buffstr, 0, i));
 	return (1);
 }
 
 int get_next_line(const int fd, char **line)
 {
+	// Ok, so here's the problem. The last part after last \n doesn't go into *line
+	// at any point because the returnhandler doesn't find \n and it doesn't even go
+	// into the read loop.
+	
 	char		buf[BUFF_SIZE + 1];
-	static char	*strs[FD_MAX];	
-	size_t		bytes;
-	int			totalread;
+	static char *strs[FD_MAX];	
+	int			bytes;
+	int			i;
 	char		*temp;
 
-	totalread = 0;
-	if (!line)
+	i = 0;
+	if ((fd < 0) || (!line) || (read(fd, buf, 0) < 0))
 		return (-1);
-	if (!strs[fd])
-		strs[fd] = "";
-	if (strs[fd])
-	{
-		youhadonejob(fd, buf, strs, line); // This is now bullshit but do this if you have stuff remaining in strs
-		return (1);
-	}
-	while ((bytes = read(fd, buf, BUFF_SIZE)) > 0)
+	*line = ft_strnew(BUFF_SIZE); // How to error check before this, is it needed?
+	//if (ft_strchr(strs[fd], '\n')) // Use returnhandler if you find newline before /0
+	//	return (returnhandler(fd, strs, line)); // returns 1, 0 or -1. When does it return -1?
+	while ((!(ft_strchr(strs[fd], '\n'))) && ((bytes = read(fd, buf, BUFF_SIZE)) > 0))
 	{
 		buf[bytes] = '\0';
-		if (youhadonejob(fd, buf, strs, line) != 0)
+		//returnvalue = linejoiner(fd, buf, strs, line);
+		// 0  = newline found
+		// 1  = no newline, got to end of buffer
+		// -1 = \0 found
+		/*if (returnvalue == -1)
+		{
+			ft_strdel(&strs[fd]);
+			return (0);
+		}
+		if (returnvalue == 0)
+			return (1);*/
+		while ((buf[i] != '\0') && (buf[i] != '\n'))
+			i++;
+		if (i == BUFF_SIZE)
+			*line = ft_strjoin(*line, buf);
+		else
+		{
+			*line = ft_strjoin(*line, ft_strsub(buf, 0, i));
+			if (buf[i] == '\n')
+			{
+				temp = ft_strdup(buf + i + 2);
+				strs[fd] = ft_strdup(temp);
+			}
 			return (1);
-		//printf("\nCurrent line: %s", line[fd]);
-		//write(1, &buf, BUFF_SIZE);
-		totalread += bytes;
-		//Break somewhere
+		}
 	}
-	return (youhadonejob(fd, buf, strs, line));
-	//printf("\nOutput:\n\n %s\n", strs[fd]);
-	//printf("\nBytes read: %d\n", totalread);
-	//write(1, strs[fd], BUFF_SIZE);
+	return (linejoiner(fd, buf, strs, line));
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tlouekar <tlouekar@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/07 10:48:15 by tlouekar          #+#    #+#             */
-/*   Updated: 2019/11/19 11:47:44 by tlouekar         ###   ########.fr       */
+/*   Updated: 2019/11/27 20:40:02 by tlouekar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,80 @@
 #include <unistd.h>
 #include "libft/libft.h"
 
+/*
 int		main(int argc, char **argv)
 {
 	int			fd;
 	char		*line;
 	int			linecount;
+	int			returnvalue;
+	int			switcher;
+	int			i;
+	int			countdown;
+	char		*fds;
 
+	i = 1;
+	countdown =  30;
+	switcher = 3;
+	returnvalue = 0;
 	linecount = 0;
-	fd = open(argv[1], O_RDONLY);
-	//printf("%s\n", lines[0]);
-
-	if(fd == -1)
-		write(1, "No file.", 8);
-	while (get_next_line(fd, &line) == 1)
+	fds = ft_strnew(argc + 2);
+	while (i < argc)
 	{
-		linecount++;
-		printf("\nCurrent line: %s", line);
-		free(line);
+		fd = open(argv[i], O_RDONLY);
+		fds[fd - 3] = 48; 
+		i++;
 	}
-	printf("File descriptor: %d, Files read: %d, Lines read: %d\n", fd, argc - 1, linecount);
+	//printf("%s\n", lines[0]);
+	while (countdown > 0)
+	{
+		while ((fds[fd - 3] == 49) && (fd < argc + 1))
+			fd++;
+		while (get_next_line(1000, &line) == 1)
+		{
+			linecount++;
+			ft_putstr(line);
+			ft_putchar('\n');
+			free(line);
+			if (fd < argc + 1)
+				fd++;
+			else
+				fd = 3;	
+		}
+		fds[fd - 3] = 49;
+		countdown--;
+	}
+	if (get_next_line(fd, &line) == -1)
+	{
+		returnvalue = -1;	
+	}
+	if (get_next_line(fd, &line) == 0)
+	{
+		returnvalue = 0;	
+	}
+	printf("Return value: %d, File descriptor: %d, Argc count: %d, Lines read: %d\n", returnvalue, fd, argc - 1, linecount);	
 	return (0);
 }
+*/
+
+int		main(void)
+{
+	char		*line;
+	int			ret;
+	int			fd;
+
+	fd = open("content1.txt", O_RDONLY);
+	//fd = 42;
+	//fd = open("gnl7_1.txt", O_RDONLY);
+	while ((ret = get_next_line(fd, &line)) > 0)
+	{
+		ft_putstr(line);
+		ft_putchar('\n');
+		//ft_strdel(&line);
+	}
+	printf("\nReturn value from gnl: %d\n", ret);
+	printf("Simple test returns on last line: %d\n", get_next_line(fd, &line));
+	close(fd);
+	return (0);
+}
+
